f861250dbe7b74c70d8a3fbc28da66af8d785550
sehako <dhtpgkr1999@gmail.com>
2025-11-01 23:18:54 +0900

    test(error-message): 에러 메시지 접두사에 대한 테스트 추가

---
+++ src/test/java/lotto/exception/common/ErrorMessageTest.java
@@ -0,0 +1,21 @@
+package lotto.exception.common;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ErrorMessageTest {
+
+    @Test
+    @DisplayName("에러 메시지는 [ERRROR] 접두사가 붙어서 출력된다.")
+    public void errorMessageTest() {
+        // given
+        ErrorMessage errorMessage = ErrorMessage.INVALID_INPUT;
+        // when
+        // then
+        Assertions.assertThat(errorMessage.getMessage())
+                .startsWith("[ERROR]");
+        Assertions.assertThat(errorMessage.getMessage())
+                .isEqualTo("[ERROR] 숫자만 입력 가능합니다.");
+    }
+}
