24dad2008be54585760898ccdd5b0afbdc339786
sehako <dhtpgkr1999@gmail.com>
2025-11-02 17:33:16 +0900

    feat(validation): 사용자가 1에서 45 사이가 아닌 다른 값을 입력했을 때의 오류 처리 기능 구현

--- src/main/java/lotto/parser/BonusNumberParser.java
+++ src/main/java/lotto/parser/BonusNumberParser.java
@@ -1,10 +1,23 @@
 package lotto.parser;
 
+import lotto.exception.InvalidInputException;
+import lotto.exception.common.ErrorMessage;
+import lotto.validator.LottoNumberValidator;
 import lotto.validator.NumberInputValidator;
 
 public class BonusNumberParser {
     public static int parse(String bonusNumberString) {
         NumberInputValidator.validate(bonusNumberString);
-        return Integer.parseInt(bonusNumberString);
+        int bonusNumber = convertToInteger(bonusNumberString);
+        LottoNumberValidator.validate(bonusNumber);
+        return bonusNumber;
+    }
+
+    private static int convertToInteger(String bonusNumberString) {
+        try {
+            return Integer.parseInt(bonusNumberString);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(ErrorMessage.INVALID_LOTTO_NUMBER);
+        }
     }
 }
---
+++ src/test/java/lotto/parser/BonusNumberParserTest.java
@@ -0,0 +1,33 @@
+package lotto.parser;
+
+import lotto.exception.common.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberParserTest {
+    @Test
+    @DisplayName("사용자가 올바른 보너스 번호를 입력하면 int형으로 변환된다.")
+    public void parseBonusNumberTest() {
+        // given
+        String input = "3";
+
+        // when
+        int bonusNumber = BonusNumberParser.parse(input);
+        // then
+        Assertions.assertThat(bonusNumber).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사용자가 1 ~ 45 사이가 아닌 보너스 번호를 입력하면 오류가 발생한다.")
+    public void invalidBonusNumberInputTest() {
+        // given
+        String input = "46";
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> BonusNumberParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
+    }
+}
