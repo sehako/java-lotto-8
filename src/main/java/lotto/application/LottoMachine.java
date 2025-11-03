cf68c22f6f877edce53a42b6d4d159d73ecb4d56
sehako <dhtpgkr1999@gmail.com>
2025-11-03 14:19:46 +0900

    refactor(lotto): 로또 발급에 대한 매직 넘버 상수화

--- src/main/java/lotto/application/LottoMachine.java
+++ src/main/java/lotto/application/LottoMachine.java
@@ -7,6 +7,9 @@ import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 
 public class LottoMachine {
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
     public Lottos issueLottos(int price) {
         List<Lotto> lottoList = IntStream.range(0, price)
@@ -17,7 +20,8 @@ public class LottoMachine {
     }
 
     private Lotto issueRandomLotto() {
-        List<Integer> integers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> integers = Randoms
+                .pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBER_COUNT);
         return new Lotto(integers);
     }
 }
