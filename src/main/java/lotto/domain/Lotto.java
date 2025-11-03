13bcfd98e2d34e9eb67966c79a963a13bf5b3288
sehako <dhtpgkr1999@gmail.com>
2025-11-03 12:37:05 +0900

    refactor(lotto): Lotto 도메인에 대한 검증 로직 추가, 매직 넘버 삭제, 메서드 구조 개선

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static lotto.domain.common.ErrorMessage.INVALID_LOTTO_NUMBER;
 import static lotto.domain.common.ErrorMessage.LOTTO_NUMBER_DUPLICATION;
 import static lotto.domain.common.ErrorMessage.NOT_SIX_LOTTO_NUMBERS;
 
@@ -9,6 +10,9 @@ import java.util.stream.Collectors;
 
 public class Lotto {
     private static final String LOTTO_NUMBER_DELIMITER = ", ";
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,13 +20,29 @@ public class Lotto {
         this.numbers = sortNumbers(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(NOT_SIX_LOTTO_NUMBERS.getMessage());
-        }
-
+    private static void validateLottoNumberUnique(List<Integer> numbers) {
         if (new HashSet<>(numbers).size() != numbers.size()) {
             throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATION.getMessage());
+        }
+    }
+
+    private static void validateLottoNumberRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (LOTTO_START_NUMBER > number || number > LOTTO_END_NUMBER) {
+                throw new IllegalArgumentException(NOT_SIX_LOTTO_NUMBERS.getMessage());
+            }
+        });
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateLottoNumberCount(numbers);
+        validateLottoNumberUnique(numbers);
+        validateLottoNumberRange(numbers);
+    }
+
+    private void validateLottoNumberCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER.getMessage());
         }
     }
 
@@ -37,11 +57,15 @@ public class Lotto {
                 .filter(lotto.numbers()::contains)
                 .count();
 
-        if (count == 5) {
+        if (isMatchFiveNumbers(count)) {
             return LottoRank.from(count, numbers.contains(bonusNumber));
         }
 
         return LottoRank.from(count, false);
+    }
+
+    private boolean isMatchFiveNumbers(int count) {
+        return count == 5;
     }
 
     public List<Integer> numbers() {
